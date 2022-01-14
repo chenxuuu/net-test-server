70c148036e7e4744486d77728f34c02729a43cb0
chenxuuu <lolicon@papapoi.com>
2022-01-14 11:16:51 +0800

    add: 能动态分tcp服务器端口号了

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
-use std::sync::{mpsc::Receiver, Arc};
+use std::{sync::{mpsc::Receiver, Arc}, net::Ipv4Addr};
 
 use futures_util::{FutureExt, SinkExt, StreamExt};
 use tokio::{
@@ -15,31 +15,48 @@ use tokio::{
     time,
 };
 use warp::{ws::Message, Filter};
+use lazy_static::*;
 
 #[derive(Debug)]
 enum Socket2Ws {
-    Connected(String),
-    Disconnected(String),
-    SocketMessage(Vec<u8>),
-    WsMessage(String),
-    Quit,
+    Created(u16),           //tcp通道开启，带端口
+    Connected(String),      //tcp客户端连上
+    Disconnected(String),   //tcp客户端断开
+    SocketMessage(Vec<u8>), //收到tcp消息
+    WsMessage(String),      //收到ws消息
+    Quit,   //wx断开连接
 }
 
 #[derive(Debug)]
 enum Ws2Socket {
-    WsMessage(String),
-    Kick(String),
-    Quit,
+    WsMessage(String),  //收到ws消息
+    Kick(String),       //某客户端要被踢了
+    Quit,               //完全退出
+}
+
+lazy_static! {
+    //存放已打开的端口
+    static ref TCP_PORTS: Mutex<[bool;65535]> = {
+        Mutex::new([false;65535])
+    };
 }
 
 async fn handle_ws_client(websocket: warp::ws::WebSocket) {
     info!("new client");
     let (mut sender, mut receiver) = websocket.split();
+    //给websocket通信用
     let (wt, mut wr) = tokio::sync::mpsc::channel(32);
     tokio::spawn(async move {
+        let mut port = 0;
         while let Some(message) = wr.recv().await {
             info!("received msg {:?}", message);
             match message {
+                Socket2Ws::Created(s) => {
+                    port = s;
+                    sender.send(Message::text(format!("port: {}", s)))
+                    .await
+                    .unwrap()
+                }
                 Socket2Ws::Connected(s) => sender
                     .send(Message::text(format!("connect: {}", s)))
                     .await
@@ -54,18 +71,50 @@ async fn handle_ws_client(websocket: warp::ws::WebSocket) {
                 Socket2Ws::WsMessage(s) => {
                     //todo!()
                 }
-                Socket2Ws::Quit => return, //断开了
+                Socket2Ws::Quit => {
+                    if port != 0 {
+                        let mut ports = TCP_PORTS.lock().await;
+                        ports[port as usize] = false;
+                    }
+                    return
+                }, //断开了
             };
         }
     });
 
+    //完全退出tcp服务端
     let (kill_all_tx, kill_all_rx) = tokio::sync::watch::channel(false);
-    let wts = wt.clone();
+    //tcp给ws发消息用
+    let mut wts = wt.clone();
+    //给tcp服务端主线程用
     let mut krm = kill_all_rx.clone();
     tokio::spawn(async move {
-        select! {
+        select! {//用select可以强退
             _ = async {
-                let mut listener = TcpListener::bind("127.0.0.1:23333").await.unwrap();
+                let mut port : u16 = 0;
+                {
+                    let mut ports = TCP_PORTS.lock().await;
+                    for i in 10000..65535 {
+                        if !ports[i] {
+                            ports[i] = true;
+                            port = i as u16;
+                            break
+                        }
+                    }
+                }
+                if port == 0{
+                    //fail todo
+                }
+                let mut listener = match TcpListener::bind((Ipv4Addr::new(127, 0, 0, 1), port)).await {
+                    Ok(l) => {
+                        wts.send(Socket2Ws::Created(port)).await.unwrap_or(());
+                        l
+                    },
+                    Err(_) => {
+                        //fail todo
+                        return
+                    }
+                };
                 loop {
                     let (mut socket, _) = listener.accept().await.unwrap();
                     let mut wtc = wts.clone();
@@ -110,7 +159,7 @@ async fn handle_ws_client(websocket: warp::ws::WebSocket) {
                     });
                 }
             } => {}
-            _ = async {
+            _ = async {//强退任务用，别的同理
                 while let Some(value) = krm.recv().await {
                     println!("received = {:?}", value);
                     if value {
@@ -135,11 +184,11 @@ async fn handle_ws_client(websocket: warp::ws::WebSocket) {
                 String::from_utf8(message.into_bytes()).unwrap(),
             ))
             .await
-            .unwrap();
+            .unwrap_or(());
         }
     }
-    kill_all_tx.broadcast(true).unwrap();
-    wt.send(Socket2Ws::Quit).await.unwrap();
+    kill_all_tx.broadcast(true).unwrap_or(());
+    wt.send(Socket2Ws::Quit).await.unwrap_or(());
 
     info!("client disconnected");
 }
@@ -155,6 +204,6 @@ async fn main() {
             // And then our closure will be called when it completes...
             ws.on_upgrade(handle_ws_client)
         });
-
+    info!("ws://127.0.0.1:2333/ws/netlab");
     warp::serve(routes).run(([127, 0, 0, 1], 2333)).await;
 }
